3e07bdb7e8b3ba14b1749f2aca094483efc26319
camilo <velandiacamilo356@gmail.com>
2020-06-06 11:55:23 -0500

    first view app

---
+++ src/devtools.js
@@ -0,0 +1,8 @@
+import { enableLiveReload } from "electron-compile";
+import electronDebug from "electron-debug";
+
+module.exports = function devtools() {
+  enableLiveReload();
+  // Mostrando las herramientas de DevTools para las diferentes ventanas
+  electronDebug({ showDevTools: true });
+};
---
+++ src/index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// instanciando los objetos app y BrowserWindow
+import { app, BrowserWindow } from "electron";
+import devtools from "./devtools";
+
+if (process.env.NODE_ENV === "development") {
+  devtools();
+}
+
+// imprimiendo un mensaje en la consola antes de salir
+app.on("before-quit", () => {
+  console.log("Saliendo..");
+});
+
+// Ejecutando ordenes cuando la aplicación esta lista
+app.on("ready", () => {
+  // creando una ventana
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "Hola Mundo!",
+    center: true,
+    maximizable: false,
+    show: false,
+  });
+
+  // Mostrando la ventana solo cuando el contenido a mostrar sea cargado
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+
+  // Escuchando el evento cuando la ventana es movida
+  win.on("move", () => {
+    const position = win.getPosition();
+    console.log(`la posición es ${position}`);
+  });
+
+  // detectando el cierre de la ventana para cerrar el aplicativo
+  win.on("closed", () => {
+    win = null;
+    app.quit();
+  });
+
+  // Carga una url desde el folder renderer
+  win.loadURL(
+    `file://${__dirname}/renderer/index.html`
+  );
+});
